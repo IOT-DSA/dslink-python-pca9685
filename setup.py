30ef96bef8ccc074d4ae9b206fd5adb1bddcd660
Logan Gorence <loganjohngorence@gmail.com>
2016-06-13 15:14:15 -0700

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,24 +1,16 @@
-from ez_setup import use_setuptools
-use_setuptools()
-from setuptools import setup, find_packages
+from setuptools import setup
 
-classifiers = ['Development Status :: 4 - Beta',
-               'Operating System :: POSIX :: Linux',
-               'License :: OSI Approved :: MIT License',
-               'Intended Audience :: Developers',
-               'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3',
-               'Topic :: Software Development',
-               'Topic :: System :: Hardware']
-
-setup(name              = 'Adafruit_PCA9685',
-      version           = '1.0.0',
-      author            = 'Tony DiCola',
-      author_email      = 'tdicola@adafruit.com',
-      description       = 'Python code to use the PCA9685 PWM servo/LED controller with a Raspberry Pi or BeagleBone Black.',
-      license           = 'MIT',
-      classifiers       = classifiers,
-      url               = 'https://github.com/adafruit/Adafruit_Python_PCA9685/',
-      dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
-      install_requires  = ['Adafruit-GPIO>=0.6.5','dslink >=0.5.8, <=0.6.15', 'twisted>=0.0.0'],
-      packages          = find_packages())
+setup(
+    name="dslink-python-pca9685",
+    version="0.1.0",
+    description="DSLink for Adafruit PCA9685",
+    url="https://github.com/IOT-DSA/dslink-python-9685",
+    author="Dennis Khvostionov",
+    author_email="dennisk@dglogik.com",
+    license="Apache 2.0",
+    install_requires=[
+        "dslink >=0.5.15, <0.6.0",
+        "Adafruit-PCA9685 == 1.0.0",
+        "Adafruit-GPIO == 1.0.0"
+    ]
+)
